ca1b74aea4104150e154573addbcf4c7e22c5742
molvlad <molvlad@frtk.ru>
2019-04-12 09:14:37 +0300

    create FrontElement.h

---
+++ include/FrontElement.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include"Vector2.h"
+
+using namespace my_math;
+
+class FrontElement : Element {
+ public:
+  FrontElement() = delete;
+
+  explicit FrontElement(const Vector2 & position);
+
+  override bool Draw() const = 0;
+
+  override bool Dump() const = 0;
+
+  bool SetPhase(const float phase);
+
+  bool SetAmplitude(const float amplitude);
+
+  virtual ~Source();
+
+ private:
+  float amplitude_;
+}
