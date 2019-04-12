3e04dc5934645cea3694924c81f531446ea7ae40
molvlad <molvlad@frtk.ru>
2019-04-12 14:26:53 +0300

    create empty functions

---
+++ src/Store.cpp
@@ -0,0 +1,35 @@
+#include"Store.h"
+
+Store::Store()
+{
+
+}
+
+Store::~Store()
+{
+
+}
+
+// need to create
+bool Store::Push(const Dipole & dipole)
+{
+
+}
+
+// need to create
+bool Store::Push(const FrontElement & front_element)
+{
+
+}
+
+// need to create
+bool Store::Draw() const
+{
+
+}
+
+// need to create
+bool Store::Dump() const
+{
+
+}
