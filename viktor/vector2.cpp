8f0824409d00acefabe1ab7c10ba1831dae2af3b
Vikotor_Torgunakov_715_FRTK <39098751+ViktorTorgunakov715FRTK@users.noreply.github.com>
2019-04-13 23:38:53 +0300

    Update Vector2.cpp
    Create functions Len( ), Reset( ), Rotate( ), Dump( ). All functions were tested.

--- viktor/vector2.cpp
+++ viktor/vector2.cpp
@@ -30,6 +30,12 @@ KernelVector2 & KernelVector2::operator=(const KernelVector2 && vect) {
   return *this;
 }
 
+void KernelVector2::Dump(void) const
+{
+    std::cout << "x_ = " << x_ << " y_ = " << y_ << std::endl;
+    return;
+}
+
 KernelVector2 & KernelVector2::operator+=(const KernelVector2 & vect) {
   x_ += vect.x_;
   y_ += vect.y_;	
@@ -40,6 +46,29 @@ KernelVector2 & KernelVector2::operator*=(const VECTOR_TYPE  k) {
   x_ *= k;
   y_ *= k;	
   return *this;
+}
+
+VECTOR_TYPE  KernelVector2::Len(void) const {
+  return sqrt(x_ * x_ + y_ * y_);
+}
+
+void KernelVector2::Reset(const VECTOR_TYPE  x, const VECTOR_TYPE  y) {
+  x_ = x;
+  y_ = y;
+  return;
+}
+
+void KernelVector2::Rotate(const VECTOR_TYPE degree) {
+  if (!x_ && !y_) {
+  	return;
+  }
+
+  VECTOR_TYPE angle = atan2(y_, x_);
+  angle += (degree * PI) / 180;
+  VECTOR_TYPE len = Len( );
+  x_ = len * cos(angle);
+  y_ = len * sin(angle);
+  return;	
 }
 
 };
