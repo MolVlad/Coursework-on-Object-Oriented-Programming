004ce097e7cf53a083139678cba775444d462b22
Vikotor_Torgunakov_715_FRTK <39098751+ViktorTorgunakov715FRTK@users.noreply.github.com>
2019-04-13 22:44:31 +0300

    Create vector2.cpp

---
+++ viktor/vector2.cpp
@@ -0,0 +1,31 @@
+#include "vector2.h"
+#include <iostream>
+
+namespace Modulation {
+
+KernelVector2::KernelVector2(void)
+    :  x_(0),
+       y_(0) {
+  }
+
+KernelVector2::KernelVector2(VECTOR_TYPE  x, VECTOR_TYPE  y)
+    :  x_(x),
+       y_(y)  {
+}
+
+KernelVector2::KernelVector2(const KernelVector2 & vect)
+    :  x_(vect.x_),
+       y_(vect.y_) {
+}   
+
+KernelVector2 & KernelVector2::operator=(const KernelVector2 & vect) {
+  x_ = vect.x_;
+  y_ = vect.y_;
+}
+
+KernelVector2 & KernelVector2::operator=(const KernelVector2 && vect) {
+  x_ = vect.x_;
+  y_ = vect.y_;
+}
+
+};
