06a3bcea59c53e6d8371d2d5aae962059542d352
molvlad <molvlad@frtk.ru>
2019-04-14 21:53:43 +0300

    vector2 complete

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 PROJECT = sfml
 
-SOURCES = src/main.cpp src/Element.cpp src/Store.cpp src/Sources.cpp src/FrontElement.cpp
+SOURCES = src/main.cpp src/Element.cpp src/Store.cpp src/Sources.cpp src/FrontElement.cpp src/Vector2.cpp
 INCLUDES += -I include
 LIBS = -lsfml-graphics -lsfml-window -lsfml-system
 DEFINES =
--- include/Vector2.h
+++ include/Vector2.h
@@ -1,47 +1,85 @@
 #pragma once
+#include <iostream>
+#include <cmath>
 
-#include<iostream>
+namespace Modulation {
 
-namespace my_math {
+const float PI = 3.141593;
+typedef float VECTOR_TYPE;
 
-class Vector2
-{
+
+class KernelVector2 {
  public:
-  Vector2(const Vector2 &v);
+  // Base functions.
+  KernelVector2(void);
+  KernelVector2(VECTOR_TYPE  x, VECTOR_TYPE  y);
+  ~KernelVector2(void) {
+  }
 
-  Vector2(const float x = 0, const float y = 0);
+  KernelVector2(const KernelVector2 & vect);
+  KernelVector2 & operator=(const KernelVector2 & vect);
+  KernelVector2 & operator=(const KernelVector2 && vect);
+  void Dump(void) const;
+  // Interface functions.
 
-  float GetLength() const;
+  // Just adding vectors.
+  KernelVector2 & operator+=(const KernelVector2 & vect);
 
-  float GetSquareLength() const;
+  // Multiply by k.
+  KernelVector2 & operator*=(const VECTOR_TYPE  k);
 
-  Vector2 GetNormalized() const;
+  // Additional functions.
+  VECTOR_TYPE  Len(void) const;
+  void Reset(const VECTOR_TYPE  x, const VECTOR_TYPE  y);
+  void Rotate(const VECTOR_TYPE  degree);
 
-  Vector2 GetPerpendicular() const;
-
-  Vector2 operator+(const Vector2& other) const;
-
-  void operator+=(const Vector2& other);
-
-  Vector2 operator-(const Vector2& other) const;
-
-  void operator-=(const Vector2& other);
-
-  Vector2 operator*(const float k) const;
-
-  Vector2 operator/(const float k) const;
-
-  ~Vector2();
-
-
- private:
-  float x_, y_;
+ protected:
+  VECTOR_TYPE  x_;
+  VECTOR_TYPE  y_;
 };
 
-std::ostream& operator<<(std::ostream& stream, const Vector2& v);
 
-std::istream& operator>>(std::istream& stream, Vector2& v);
+// Shell class
+class Vector2 : public KernelVector2 {
+ public:
+  // Base functions.
+  Vector2(void) 
+      : KernelVector2( ) {
+  }
 
-Vector2 operator*(float k, const Vector2& v);
+  Vector2(VECTOR_TYPE  x, VECTOR_TYPE  y)
+      :  KernelVector2(x, y) {
+  }
 
-} /* namespace my_math */
+  ~Vector2(void) {
+  }
+
+  // Just adding vectors.
+  Vector2 operator+(const Vector2 & vect) const;
+
+  // Just subtracting vectors. 
+  Vector2 & operator-=(const Vector2 & vect);
+  Vector2 operator-(const Vector2 & vect) const;
+
+  // Multiply by -1.
+  Vector2 operator-(void) const;
+
+  // Multiply by k.
+  Vector2 operator*(const VECTOR_TYPE  k) const;
+
+  // Division by k. Situation 0 / 0 and (const != 0) / 0 is correct, but poison vector.
+  Vector2 operator/(const VECTOR_TYPE  k) const;
+
+  // Additional functions.
+  VECTOR_TYPE  SquareLen(void) const;
+
+  // Return rotated vector.
+  Vector2 GetRotated(const VECTOR_TYPE  degree) const;
+
+  // Length of vector is 1 now.
+  void Norm(void);
+};
+
+// Addition to override operators.
+Vector2 operator*(const VECTOR_TYPE k, const Vector2 & vect);
+};
