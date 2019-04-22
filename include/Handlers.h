165498ee9c26478d5c6c0438408e7a87a47ddcf3
molvlad <molvlad@frtk.ru>
2019-04-22 11:00:32 +0300

    mouse handler added

---
+++ include/Handlers.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+#include "Store.h"
+
+#define MOUSE_DEBUG 1
+
+bool handleEvent(sf::RenderWindow &window, Store &store);
+bool handleMouse(sf::RenderWindow &window, sf::Event event, Store store);
---
+++ src/Handlers.cpp
@@ -0,0 +1,53 @@
+#include "Handlers.h"
+
+bool handleEvent(sf::RenderWindow &window, Store &store)
+{
+	sf::Event event;
+
+	while (window.pollEvent(event))
+	{
+		switch(event.type)
+		{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::MouseButtonPressed:
+				handleMouse(window, event, store);
+				break;
+		}
+	}
+
+  return true;
+}
+
+bool handleMouse(sf::RenderWindow &window, sf::Event event, Store store)
+{
+	sf::Vector2i position = sf::Mouse::getPosition(window);
+
+	switch(event.key.code)
+	{
+		case sf::Mouse::Left:
+      #ifdef MOUSE_DEBUG
+      std::cout << "handleMouse(): Left" << std::endl;
+      #endif /* MOUSE_DEBUG */
+
+			//elements.addPosCharge(my_math::Vector2(position));
+			break;
+		case sf::Mouse::Right:
+      #ifdef MOUSE_DEBUG
+      std::cout << "handleMouse(): Right" << std::endl;
+      #endif /* MOUSE_DEBUG */
+
+			//elements.addNegCharge(my_math::Vector2(position));
+			break;
+		case sf::Mouse::Middle:
+      #ifdef MOUSE_DEBUG
+      std::cout << "handleMouse(): Middle" << std::endl;
+      #endif /* MOUSE_DEBUG */
+
+			//elements.addLine(my_math::Vector2(position));
+			break;
+	}
+
+  return true;
+}
