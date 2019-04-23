faf58875db0a4d24aa2aaaa07bcbb89832cdd383
viktor <Tor-vic2000@yandex.ru>
2019-04-23 16:22:09 +0300

    Appear only in dipole area

--- include/Handlers.h
+++ include/Handlers.h
@@ -11,3 +11,6 @@ bool HandleEvent(sf::RenderWindow &window, Store &store);
 bool HandleMouse(sf::RenderWindow &window, sf::Event event, Store &store);
 bool HandleDraw(sf::RenderWindow &window, Store &store);
 bool HandleStore(Store &store);
+
+// Don't set dipoles inside dipole area.
+bool IsRightPosition(const sf::Vector2i & position);
