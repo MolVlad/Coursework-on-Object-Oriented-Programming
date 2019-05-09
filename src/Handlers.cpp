6096708381db184f810132da5847c62755f37f0e
viktor <Tor-vic2000@yandex.ru>
2019-05-09 20:55:11 +0300

    THe last commit for today!

--- src/Handlers.cpp
+++ src/Handlers.cpp
@@ -265,7 +265,7 @@ bool HandleKey(const sf::RenderWindow &window, const sf::Event &event, Store & s
       #ifdef KEY_DEBUG
       std::cout << "HandleKey( ): Space" << std::endl;
       #endif /* KEY_DEBUG */
-      DiffractionGrating diffraction_grating = DiffractionGrating(my_math::Vector2(position), 100., 10., 2);
+      DiffractionGrating diffraction_grating = DiffractionGrating(my_math::Vector2(position), 40., 10., 10);
       store.Push(diffraction_grating);
       break;      
   }
